87eb2b31d9e578271cbe8807fe7843fa89c04c66
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-02-07 10:33:40 +0300

    reformat code

--- Exam/Res.java
+++ Exam/Res.java
@@ -1,79 +1,88 @@
 
-class MultiCalcul{
+class MultiCalcul {
 
     protected float a;
     protected float b;
     protected float result;
-    MultiCalcul(){
-        a=0;
-        b=0;
-    }
-    MultiCalcul(float u, float v){
-        a=u;
-        b=v;
-    }
-float compute(float u,float v)
-{
 
-    return result;
-}
-    void  show(){
-
+    MultiCalcul() {
+        a = 0;
+        b = 0;
     }
 
-}
-class DerivProduct extends MultiCalcul{
-    DerivProduct()
-    {
-        super();
+    MultiCalcul(float u, float v) {
+        a = u;
+        b = v;
     }
-    DerivProduct(float u,float v){
-        super(u,v);
 
-    }
- float compute(float u,float v){
-        super.compute(u,v);
-        float res;
-        res= u*v;
-        return res;
-    }
-    void show(){
+    float compute(float u, float v) {
 
-        System.out.print(a+"*"+b+"=");
-    }
-}
-class DerivSum extends MultiCalcul{
-
-    DerivSum(){
-
-        super();
-    }
-    DerivSum(float u,float v)
-    {
-        super(u,v);
-
-    }
-    float compute(float u,float v)
-    {
-        super.compute(u,v);
-        result= u+v;
         return result;
     }
-    void show(){
 
-        System.out.print(a+"+"+b+"=");
+    void show() {
+
+    }
+
+}
+
+class DerivProduct extends MultiCalcul {
+    DerivProduct() {
+        super();
+    }
+
+    DerivProduct(float u, float v) {
+        super(u, v);
+
+    }
+
+    float compute(float u, float v) {
+        super.compute(u, v);
+        float res;
+        res = u * v;
+        return res;
+    }
+
+    void show() {
+
+        System.out.print(a + "*" + b + "=");
     }
 }
+
+class DerivSum extends MultiCalcul {
+
+    DerivSum() {
+
+        super();
+    }
+
+    DerivSum(float u, float v) {
+        super(u, v);
+
+    }
+
+    float compute(float u, float v) {
+        super.compute(u, v);
+        result = u + v;
+        return result;
+    }
+
+    void show() {
+
+        System.out.print(a + "+" + b + "=");
+    }
+}
+
 public class Res {
     public static void main(String[] args) {
-     float result;
+        float result;
         MultiCalcul point[] = new MultiCalcul[2];
-        point[0] = new DerivProduct(4,3);
-        point[1]= new DerivSum(4,3);
-    for(int i=0 ;i<2;i++) {
-    result = point[i].compute(4,3);
-    point[i].show();
-    System.out.println(result);
-     }
+        point[0] = new DerivProduct(4, 3);
+        point[1] = new DerivSum(4, 3);
+        for (int i = 0; i < 2; i++) {
+            result = point[i].compute(4, 3);
+            point[i].show();
+            System.out.println(result);
+        }
     }
 }
