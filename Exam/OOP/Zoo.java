3787e9e5685ff32442a5e83f35461137669fcbab
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-02-16 18:55:54 +0300

    learning OOP

---
+++ Exam/OOP/Zoo.java
@@ -0,0 +1,10 @@
+public class Zoo {
+    public static void main(String [] args){
+        Animal animal1= new Animal(12,"M",23);
+        animal1.eat();
+        Animal bird1= new Bird(3,"F",10);
+        bird1.eat();
+        bird1.sleep();
+
+    }
+}
